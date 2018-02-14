3802478dc339287326ede318f8a2ec454fc63ea5
unknown <drapatiy92@gmail.com>
2018-02-14 17:58:45 +0200

    aaaa

--- app.js
+++ app.js
@@ -27,6 +27,27 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 
+app.use((req, res, next) => {
+    const err = new Error(`Not Found ${req.path}`)
+    err.status = 404
+next(err)
+})
+app.use((error, req, res, next) => {
+    if (error) {
+        console.log(error)
+        return res.status(400).json({error})
+    }
+    next(error)
+})
+app.use((err, req, res, next) => {
+    res.status(err.status || 500)
+res.render('error', {
+    message: err.message,
+    error: {}
+})
+})
+
+
 app.get("/", function (req, res) {
     // Items.find({}, function (err, result) {
     //     if (!err) {
